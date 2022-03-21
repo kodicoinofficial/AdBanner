d51c017f31872a8c2802ea93c7ee262782fd056d
KODI <team@kodicoin.com>
2022-03-21 19:34:02 -0400

    iFrame AdBanner

---
+++ src/Banner.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "./Banner.css";
+
+function App() {
+  return (
+  
+    <div class="videoWrapper">
+      <div class="videoWrapper2">
+   <iframe src="https://cookiesale.app/_ADS/_resources/ads.html" frameBorder="0"  scrolling="no" marginwidth="0"
+ marginheight="0"
+ hspace="0"
+ vspace="0"
+ frameborder="0"></iframe>
+</div>
+</div>
+ );
+}
+
+export default App;
